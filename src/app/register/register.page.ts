47aed243440976919bccfbe357edce2a2669aa79
georges tchianga <georgestcm@gmail.com>
2019-05-15 00:27:40 -0400

    import router and saved icon asset in property

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
-
-  constructor() { }
+public  iconLink: string ="assets/imgs/icon.png";
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
