9dac11ff4ee640252aa41ba6b7de513210592281
georges tchianga <georgestcm@gmail.com>
2019-06-28 20:39:00 -0400

    subscription service added and router added for navigation

--- src/app/dashboardComponents/home/home.page.ts
+++ src/app/dashboardComponents/home/home.page.ts
@@ -5,6 +5,8 @@ import { Storage } from '@ionic/storage';
 import { PhotoViewer } from '@ionic-native/photo-viewer/ngx';
 import { AlertController } from '@ionic/angular';
 import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { SubscriptionService } from 'src/app/subscription.service';
+import { Router } from '@angular/router'
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
@@ -18,16 +20,20 @@ public lat:Number;
 usersNear;
  jobsNear=[];
  allJobsNear;
-
+id;
+data;
   constructor(public _data: DataService,
     private route: ActivatedRoute,
     public storage: Storage,
     public alertController: AlertController,
-  private photoViewer: PhotoViewer) {
+  private photoViewer: PhotoViewer,
+private _subscription: SubscriptionService,
+private _router:Router) {
  }
 
   ngOnInit() {
   this.storage.get('user').then((value)=>{
+    this.id = value._id;
  this.long = value.geometry.coordinates[0];
  this.lat = value.geometry.coordinates[1];
  console.log(value)
@@ -58,6 +64,21 @@ usersNear;
       await alert.present();
     }
 
+    async alert(msg) {
+        const alert = await this.alertController.create({
+          header: '',
+          subHeader: '',
+          message: msg,
+          buttons: [{
+          text: 'Ok',
+          role: 'ok',
+          handler: () => {
+             this._router.navigate(['dashboard/payments'])
+            }
+        }] })
+
+        await alert.present();
+      }
 
  getUsers() {
   while(this.jobsNear.length>0){
@@ -74,9 +95,7 @@ usersNear;
    )
  }
 
- viewImg(src){
-   this.photoViewer.show(src);
- }
+
 
 setDistance(distance){
   this.distance = distance;
@@ -89,5 +108,22 @@ loadJobs(){
 })
 this.allJobsNear= Array.prototype.concat.apply([], this.jobsNear);
 console.log(this.allJobsNear)
+}
+
+checkSubscriptionStatusApply(){
+   this._subscription.checkSubscription(this.id)
+   .subscribe(
+     res =>(
+    this.data=res,
+    console.log(this.data.statusCode),
+    this.checkData()
+     ),
+     err => console.log(err),
+   )
+}
+checkData(){
+  if(this.data.statusCode=404){
+    this.alert("you need to subscribe to apply")
+  }
 }
 }
