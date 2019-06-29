4300bbe9d0c7be5b22951de256328302876c245d
georges tchianga <georgestcm@gmail.com>
2019-06-28 20:36:56 -0400

    stripe tokenizer info added

--- src/app/dashboardComponents/payments/payments.page.ts
+++ src/app/dashboardComponents/payments/payments.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Stripe } from '@ionic-native/stripe/ngx';
+import { SubscriptionService } from 'src/app/subscription.service'
 @Component({
   selector: 'app-payments',
   templateUrl: './payments.page.html',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaymentsPage implements OnInit {
 public  iconLink: string ="/assets/imgs/icon.png";
-  constructor() { }
+  constructor(private stripe: Stripe,private _subscription: SubscriptionService) {}
 
   ngOnInit() {
+    this.stripe.setPublishableKey('pk_test_OR9yEz19qnyImgqlHLaqBEwO00S5J9STou');
   }
+
+   card = {
+     number:Number,
+     expMonth:Number,
+     expYear:Number,
+     cvc:Number
+  }
+
+log(){
+  console.log(this.card)
+}
+createToken(card){
+  this.stripe.createCardToken(card)
+   .then(token => console.log(token.id))
+   .catch(error => console.error(error));
+}
+
 
 }
