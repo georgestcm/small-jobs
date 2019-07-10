0bb6c1874058f39a41bafeff212e207562e6fd93
georges tchianga <georgestcm@gmail.com>
2019-07-09 22:30:49 -0400

    message sending service

---
+++ src/app/sendmessage.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+@Injectable({
+  providedIn: 'root'
+})
+export class SendmessageService {
+
+  sendmessagelink = "https://quickjobsapi.herokuapp.com/api/message"
+
+  constructor(private http: HttpClient) { }
+
+  sendmessage(body,poster_number){
+    return this.http.post<any>(this.sendmessagelink,body,{
+      params: {
+        number: poster_number
+      }
+    })
+  }
+}
