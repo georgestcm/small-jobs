726ce43ce2d27ae3ef26c2ab1f9fc3613dfb3792
georges tchianga <georgestcm@gmail.com>
2019-07-09 22:32:44 -0400

    ng on init moved to ionviewWillenter

--- src/app/dashboardComponents/home/home.page.ts
+++ src/app/dashboardComponents/home/home.page.ts
@@ -6,6 +6,7 @@ import { PhotoViewer } from '@ionic-native/photo-viewer/ngx';
 import { AlertController } from '@ionic/angular';
 import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 import { SubscriptionService } from 'src/app/subscription.service';
+import { SendmessageService } from 'src/app/sendmessage.service';
 import { Router } from '@angular/router'
 @Component({
   selector: 'app-home',
@@ -16,32 +17,61 @@ export class HomePage implements OnInit {
 public distance: String;
 public long:Number;
 public lat:Number;
-
+appliedJobs;
 usersNear;
  jobsNear=[];
+ appliedjobsId= [];
  allJobsNear;
 id;
 data;
+jobToPost;
+applyOrNot: boolean;
+//jobsending
+myInfo= {
+  email:'',
+  number:'',
+  name: ''
+}
+
+poster_number;
+posterData;
+job;
+text={
+msg:''
+}
+
+
+//jobsending
   constructor(public _data: DataService,
     private route: ActivatedRoute,
     public storage: Storage,
     public alertController: AlertController,
   private photoViewer: PhotoViewer,
 private _subscription: SubscriptionService,
-private _router:Router) {
+private _router:Router,
+public _sendM: SendmessageService) {
  }
 
   ngOnInit() {
-  this.storage.get('user').then((value)=>{
-    this.id = value._id;
- this.long = value.geometry.coordinates[0];
- this.lat = value.geometry.coordinates[1];
- console.log(value)
- this.distance = "1609.34";
- this.getUsers()
- })
+
 
   }
+
+  ionViewWillEnter(){
+    this.storage.get('user').then((value)=>{
+      this.id = value._id;
+    this.long = value.geometry.coordinates[0];
+    this.lat = value.geometry.coordinates[1];
+    this.myInfo.email = value.email;
+    this.myInfo.number = value.phone_number;
+    this.myInfo.name = value.first_name +' '+ value.last_name;
+    this.distance = "1609.34";
+    this.getUsers()
+    //console.log(this.myInfo)
+    })
+
+  }
+
   doRefresh(event) {
     this.getUsers();
     setTimeout(() => {
@@ -53,6 +83,16 @@ private _router:Router) {
   viewImg(src){
     this.photoViewer.show(src);
   }
+
+
+  disablebutton(id){
+    if(this.id === id){
+      return true
+    } else {
+      return false
+    }
+  }
+
   async presentAlert(msg) {
       const alert = await this.alertController.create({
         header: 'Job Description',
@@ -62,6 +102,16 @@ private _router:Router) {
       });
 
       await alert.present();
+    }
+
+    getApplied(){
+      this._data.getAppliedJobs(this.id)
+      .subscribe(
+        res=>(
+          this.appliedJobs = res
+        ),
+        err=> console.log(err)
+      )
     }
 
     async alert(msg) {
@@ -84,7 +134,7 @@ private _router:Router) {
   while(this.jobsNear.length>0){
     this.jobsNear.pop()
   }
-
+this.getApplied()
    this._data.setPosition(this.distance,this.long,this.lat)
    .subscribe(
      res =>(
@@ -93,6 +143,7 @@ private _router:Router) {
      err => console.log(err),
      ()=> this.loadJobs()
    )
+
  }
 
 
@@ -107,15 +158,42 @@ loadJobs(){
   this.jobsNear.push(user.postedJobs)
 })
 this.allJobsNear= Array.prototype.concat.apply([], this.jobsNear);
-console.log(this.allJobsNear)
-}
+//console.log(this.appliedJobs)
+//console.log(this.allJobsNear)
+ let applied = this.appliedJobs;
+ let jobsN = this.allJobsNear;
+ let appliedId = [];
+ let jobsNearId = [];
+ for(let i=0; i<jobsN.length; i++){
+   //this.appliedId.push()
+   jobsNearId.push(this.allJobsNear[i]._id)
+
+ }
+ for(let i=0; i<applied.length; i++){
+   //this.appliedId.push()
+   appliedId.push(this.appliedJobs[i]._id)
+
+ }
+ console.log(jobsNearId)
+ console.log(appliedId)
+
+ for(let i=0; i <jobsNearId.length; i++){
+   if(jobsNearId[i] === appliedId[i]){
+     console.log(i)
+    this.allJobsNear.splice(i,1)
+    --i;
+    break;
+  console.log(i)
+   }
+ }
+ }
 
 checkSubscriptionStatusApply(){
    this._subscription.checkSubscription(this.id)
    .subscribe(
      res =>(
     this.data=res,
-    console.log(this.data.statusCode),
+    //console.log(this.data.statusCode),
     this.checkData()
      ),
      err => console.log(err),
@@ -126,4 +204,55 @@ checkData(){
     this.alert("you need to subscribe to apply")
   }
 }
+
+apply(job){
+  this.job = job;
+  console.log(job)
+  this.text.msg ='User'+' '+this.myInfo.name+' '+ 'has applied to work for you. On the job titled'+' '+`'${job.job_title}'`+' '+'The applicant contact informations are:'+' '+'Email:'+' '+this.myInfo.email+', '+'Phone number:'+' '+this.myInfo.number;
+  this._data.getCurrentUser(job.poster_id)
+    .subscribe(
+      res=>(
+        this.posterData = res,
+        this.poster_number  ='+1'+this.posterData.phone_number,
+        console.log(this.poster_number),
+        console.log(this.text),
+        this._sendM.sendmessage(this.text,this.poster_number)
+        .subscribe(
+          res=>(
+            this.presentAlert("Applied"),
+            console.log(res)
+          ),
+          err=> console.log(err)
+        )
+      ),
+      err=> console.log(err)
+    )
+    //this.poster_number ='+1';
+    //console.log(this.poster_number);
+    this._data.postApplied(job,this.id)
+    .subscribe(
+      res=>(
+        console.log(res),
+        this.presentAlert("Applied")
+      ),
+      err=> console.log(err)
+    )
+  /*
+  this._data.postApplied(job,this.id)
+  .subscribe(
+    res=>(
+      console.log(res),
+      this.presentAlert("Applied")
+    ),
+    err=> console.log(err)
+  ) */
+}
+
+checkApplied(){
+
+}
+
+
+
+
 }
