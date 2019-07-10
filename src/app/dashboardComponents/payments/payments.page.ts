7f8bd05274640846f8dea9e19937804a6c06a804
georges tchianga <georgestcm@gmail.com>
2019-07-09 22:43:08 -0400

    ts for payment manipulation

--- src/app/dashboardComponents/payments/payments.page.ts
+++ src/app/dashboardComponents/payments/payments.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Stripe } from '@ionic-native/stripe/ngx';
 import { SubscriptionService } from 'src/app/subscription.service'
+import { AlertController } from '@ionic/angular';
+import { Storage } from '@ionic/storage';
 @Component({
   selector: 'app-payments',
   templateUrl: './payments.page.html',
@@ -8,27 +10,56 @@ import { SubscriptionService } from 'src/app/subscription.service'
 })
 export class PaymentsPage implements OnInit {
 public  iconLink: string ="/assets/imgs/icon.png";
-  constructor(private stripe: Stripe,private _subscription: SubscriptionService) {}
+  constructor(private stripe: Stripe,private _subscription: SubscriptionService,public alertController: AlertController,public storage: Storage) {}
 
   ngOnInit() {
+    this.storage.get('user').then((value)=>{
+      this.id = value._id;
+   })
+
     this.stripe.setPublishableKey('pk_test_OR9yEz19qnyImgqlHLaqBEwO00S5J9STou');
   }
-
+  id;
+  token;
    card = {
-     number:Number,
-     expMonth:Number,
-     expYear:Number,
-     cvc:Number
+     number:undefined,
+     expMonth:undefined,
+     expYear:undefined,
+     cvc:undefined
   }
 
-log(){
-  console.log(this.card)
+trial(){
+  this.presentAlert("You are currently on trial and you will be promped to subscribe after the trial")
 }
-createToken(card){
-  this.stripe.createCardToken(card)
-   .then(token => console.log(token.id))
-   .catch(error => console.error(error));
+createToken(){
+  this.stripe.createCardToken(this.card)
+   .then((token)=>{
+     console.log(token);
+     this.token = token.id
+     this.createCustomer()
+   })
+   .catch(error => this.presentAlert("Please check card details before submition"));
 }
 
+async presentAlert(msg) {
+  const alert = await this.alertController.create({
+    header:  "",
+    message: msg,
+    buttons: ['OK']
+  });
 
+  await alert.present();
+}
+
+createCustomer(){
+  this._subscription.addCustomer(this.id,this.token)
+  .subscribe(
+    res=> (
+     console.log(res)
+    ),
+    err=>(
+      console.log(err)
+    )
+  )
+}
 }
