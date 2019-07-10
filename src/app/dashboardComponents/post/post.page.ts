0796decf8d684ead9091c3ce9521a5b18a54cc84
georges tchianga <georgestcm@gmail.com>
2019-07-09 22:43:48 -0400

    set img to base64

--- src/app/dashboardComponents/post/post.page.ts
+++ src/app/dashboardComponents/post/post.page.ts
@@ -24,12 +24,13 @@ location:'',
 due_date:'',
 time:'',
 price:undefined,
-images: []
+images: [],
+completed: false
  }
 
  options: CameraOptions = {
-   quality: 100,
-   destinationType: this.camera.DestinationType.FILE_URI,
+   quality: 50,
+   destinationType: this.camera.DestinationType.DATA_URL,
    encodingType: this.camera.EncodingType.JPEG,
    mediaType: this.camera.MediaType.PICTURE
  }
