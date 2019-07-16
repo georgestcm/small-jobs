733b8eafebab8a9041b8f07dd702b9912e5b68c3
georges tchianga <georgestcm@gmail.com>
2019-07-16 02:39:41 -0400

    applied declaration removed

--- src/app/dashboardComponents/applied-jobs/applied-jobs.module.ts
+++ src/app/dashboardComponents/applied-jobs/applied-jobs.module.ts
@@ -5,12 +5,10 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
 
-import { AppliedJobsPage } from './applied-jobs.page';
 
 const routes: Routes = [
   {
-    path: '',
-    component: AppliedJobsPage
+    path: ''
   }
 ];
 
@@ -21,6 +19,6 @@ const routes: Routes = [
     IonicModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [AppliedJobsPage]
+  declarations: []
 })
 export class AppliedJobsPageModule {}
