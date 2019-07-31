2ebc154a4cabeabbff1056dd9dac0a92ac437d39
georges tchianga <georgestcm@gmail.com>
2019-07-31 07:02:58 -0400

    review service added to review adding

---
+++ src/app/review.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http"
+@Injectable({
+  providedIn: 'root'
+})
+export class ReviewService {
+
+apppost = "https://quickjobsapi.herokuapp.com/api/applicantspost"
+appdelete = "https://quickjobsapi.herokuapp.com/api/applicantsdelete"
+reviewpost = "https://quickjobsapi.herokuapp.com/api/reviewpost"
+  constructor(private http: HttpClient) { }
+
+  postapplicants(id,app){
+    return this.http.post<any>(this.apppost,app,{
+      params: {
+        id:id
+      }
+    })
+  }
+
+  deleteapp(user_id,app_id){
+    return this.http.delete<any>(this.appdelete,{
+      params: {
+        id:user_id,
+        appid:app_id
+      }
+    })
+  }
+
+  postreview(user_id,review){
+    return this.http.post(this.reviewpost,review,{
+      params: {
+        id: user_id
+      }
+    })
+  }
+}
